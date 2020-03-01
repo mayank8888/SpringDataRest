0f7a69497ab0c8624895bdfef190538dfdc71b11
mayank8888 <shivastava.mayank@gmail.com>
2020-03-01 09:26:15 +0100

    Sping Data Rest

---
+++ SpringDataRest/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into alien values (100, 'Mayank', 'ABAP');
+insert into alien values (101, 'Golu', 'Java');
+insert into alien values (102, 'Rajjo', 'JS');
+insert into alien values (103, 'Mayank', 'Assembly');
+insert into alien values (104, 'Mayank', 'Java');
